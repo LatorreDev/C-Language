f61b512cec924a2b86a5bbf81e60d5e9b66192a1
David Latorre <david@latorredev.com>
2019-03-22 16:25:14 -0500

    add inOutdata file

---
+++ inOutdata.c
@@ -0,0 +1,18 @@
+//In Out Data 
+
+#include<stdio.h>
+
+int main()
+{
+    /* int a;
+    float b;
+    char c;
+
+    printf("digita el valor de la variable a:");
+    scanf("%i", &a);
+    printf("El valor de la variable es: %i",a);
+*/
+
+printf("Hello World!!!");
+    return 0;
+}
