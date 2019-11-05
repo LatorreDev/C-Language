c387e78606e737d735b4b8715d116dbe49e7ccc6
David Latorre <david@latorredev.com>
2019-11-05 01:02:03 -0500

    [UPDATE] change users name and fix broken loop

--- platziTerminal.c
+++ platziTerminal.c
@@ -4,11 +4,11 @@
 void main(){
 
 char userName[25];
-int decision;
-int bucle = 1;
-int estudiando;
+int decision = 1;
+int loop = 1;
+int estudiando = 1;
 
-while (bucle == 1){
+while (loop == 1){
 
     printf("Bienvenido a Platzi Terminal\n");
     printf("Ingresa tu nombre de usuario: \n");
@@ -16,7 +16,7 @@ while (bucle == 1){
 
     // printf("Bienvenido %s, vamos a acceder a tu sección\n", userName);
 
-    if(strcmp(userName,"Julian")==0)
+    if(strcmp(userName,"David")==0)
     {
         while(estudiando ==1){
         printf("Esto es Platzi Terminal Free Edition, ¿Qué deseas hacer?\n");
@@ -30,9 +30,9 @@ while (bucle == 1){
                 printf("Goodbye :)\n");
             estudiando = 2;
         }
-        
+
     }
-    else if(strcmp(userName,"Juliana")==0){
+    else if(strcmp(userName,"Camilo")==0){
         printf("Esto es Platzi Terminal Standard Edition, ¿Qué deseas hacer?\n");
         printf("Ingrese 1 para estudiar\nIngrese 2 para leer el blog\nIngrese cualquier otro valor para salir\n");
         scanf("%d",&decision);
@@ -42,10 +42,8 @@ while (bucle == 1){
                 printf("Estas en el blog aprovechalo\n");
             else
                 printf("Goodbye :)\n");
-
-        
     }
-    else if(strcmp(userName,"Paula")==0){
+    else if(strcmp(userName,"Angela")==0){
         printf("Esto es Platzi Terminal Premium Edition, ¿Qué deseas hacer?\n");
         printf("Ingrese 1 para estudiar\nIngrese 2 para leer el blog\nIngrese 3 para mentoria\nIngrese cualquier otro valor para salir\n");
         scanf("%d",&decision);
