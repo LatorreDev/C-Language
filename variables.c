d64ee7eb73b22ad1e8890831b428798bf6cc7048
David Latorre <david@latorredev.com>
2020-05-10 23:08:38 -0500

    [ADD] add source code of variables with linux kernel coding style

---
+++ variables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int myVariable;
+
+	myVariable = 0;
+
+	printf("%d\n", myVariable);
+
+	myVariable += 1;
+
+	printf("%d\n", myVariable);
+
+	return (0);
+}
